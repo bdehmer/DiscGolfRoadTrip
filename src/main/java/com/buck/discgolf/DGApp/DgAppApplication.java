fbae9819d92b0105d2e6eb2f6017f61c578367a3
Brandon Dehmer <brandond27@comcast.net>
2022-04-03 15:00:38 -0500

    initial commit

---
+++ src/main/java/com/buck/discgolf/DGApp/DgAppApplication.java
@@ -0,0 +1,13 @@
+package com.buck.discgolf.DGApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buck/discgolf/DGApp/DgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buck.discgolf.DGApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
